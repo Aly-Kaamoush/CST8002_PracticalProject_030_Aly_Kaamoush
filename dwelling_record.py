be862a1aafe351417a22af8c441b3a1729925c18
Aly-Kaamoush <kaam0003@algonquinlive.com>
2025-01-21 18:13:39 -0500

    Create main.py where Open the CSV file using File-IO and reading using Panadas API with error handling, Adjusting identation for dwelling _record.py

--- dwelling_record.py
+++ dwelling_record.py
@@ -1,5 +1,5 @@
 '''
-CST8002 - Python Programming
+CST8002 - Programming Language Research Project
 Professor:  Tyler DeLay
 Author: Aly Kaamoush
 Date: January 26, 2025
@@ -8,7 +8,7 @@ Description: This file contains the DwellingRecord class for storing and managin
 
 class DwellingRecord:
     ''' A class to recrd a single dwelling unit record from the dataset. '''
-    def __init_(self):
+    def __init__(self):
         ''' Initializing an empty dwelling constructor with default values.'''
         self._csduid = 0
         self._csd = ""
@@ -17,52 +17,52 @@ class DwellingRecord:
         self._unit_measure = 0.0
         self._original_value = 0.0
 
-def get_csduid(self):
-    '''Return the CSDUID value.'''
-    return self._csduid
+    def get_csduid(self):
+        '''Return the CSDUID value.'''
+        return self._csduid
 
-def set_csduid(self, value):
-    '''Set the CSDUID value.'''
-    self._csduid = value
+    def set_csduid(self, value):
+        '''Set the CSDUID value.'''
+        self._csduid = value
 
-def get_csd(self):
-    '''Return the CSD name.'''
-    return self._csd
+    def get_csd(self):
+        '''Return the CSD name.'''
+        return self._csd
 
-def set_csd(self, value):
-    '''Set the CSD name.'''
-    self._csd = value
+    def set_csd(self, value):
+        '''Set the CSD name.'''
+        self._csd = value
 
-def get_period(self):
-    '''Return the period value.'''
-    return self._period
+    def get_period(self):
+        '''Return the period value.'''
+        return self._period
 
-def set_period(self, value):
-    '''Set the period value.'''
-    self._period = value
+    def set_period(self, value):
+        '''Set the period value.'''
+        self._period = value
 
-def get_indicator(self):
-    '''Return the indicator summary description.'''
-    return self._indicator
+    def get_indicator(self):
+        '''Return the indicator summary description.'''
+        return self._indicator
 
-def set_indicator(self, value):
-    '''Set the indicator summary description.'''
-    self._indicator = value
+    def set_indicator(self, value):
+        '''Set the indicator summary description.'''
+        self._indicator = value
 
-def get_unit_measure(self):
-    '''Return the unit of measure value.'''
-    return self._unit_measure
+    def get_unit_measure(self):
+        '''Return the unit of measure value.'''
+        return self._unit_measure
 
-def set_unit_measure(self, value):
-    '''Set the unit of measure value.'''
-    self._unit_measure = value
+    def set_unit_measure(self, value):
+        '''Set the unit of measure value.'''
+        self._unit_measure = value
 
-def get_original_value(self):
-    '''Return the original value.'''
-    return self._original_value
+    def get_original_value(self):
+        '''Return the original value.'''
+        return self._original_value
 
-def set_original_value(self, value):
-    '''Set the original value.'''
-    self._original_value = value
+    def set_original_value(self, value):
+        '''Set the original value.'''
+        self._original_value = value
 
 
---
+++ main.py
@@ -0,0 +1,47 @@
+'''
+CST8002 - Programming Language Research Project
+Professor:  Tyler DeLay
+Author: Aly Kaamoush
+Date: January 26, 2025
+Description: Main Program file for reading and displaying dwelling unit data
+'''
+from dwelling_record import DwellingRecord
+import pandas as pd
+
+def read_dwelling_data(filename):
+    ''' Open the CSV file using File-IO and reading using Panadas API with error handling'''
+    my_records = []
+
+    try:
+        with open(filename, 'r') as file:
+
+            data = pd.read_csv(filename)
+
+            first_few = data.head()
+
+            rows = first_few.values.tolist()
+
+            for row in rows:
+
+                new_record = DwellingRecord()
+
+                new_record.set_csduid(row[0])
+                new_record.set_csd(row[1])
+                new_record.set_period(row[2])
+                new_record.set_indicator(row[3])
+                new_record.set_unit_measure(row[4])
+                new_record.set_original_value(row[5])
+
+                my_records.append(new_record)
+
+    except FileNotFoundError:
+        print("Error: can't find the csv file")
+    except Exception as e:
+        print ("Something went wrong: ", e)
+
+    return my_records
+
+
+
+
+
